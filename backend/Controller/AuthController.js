7b34cbd81aac92ba00e111d5921999a7b19bc338
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-25 04:24:13 +0500

    Registraiton of the user is now workign as inteded

--- backend/Controller/AuthController.js
+++ backend/Controller/AuthController.js
@@ -1,16 +1,31 @@
-function Login(req, res) {
+const bcypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const db = require('../db/queryAuth');
+
+async function Login(req, res) {
 	try {
 		res.status(200).json('sadsadsa Server Error');
 	} catch (error) {
 		res.status(500).json('Internal Server Error', error);
 	}
 }
-function Register(req, res) {
+async function Register(req, res) {
+	const { username, email, password, role, institute_id } = req.body;
 	try {
-    		res.status(200).json('sadsadsa Server Error');
-
+		const password_hash = await bcypt.hash(password, 10);
+		const newUser = await db.registerQuery(
+			username,
+			email,
+			password_hash,
+			role,
+			institute_id,
+		);
+		res.status(201).json({ message: 'New user registered', user: newUser });
 	} catch (error) {
-		res.status(500).json('Internal Server Error', error);
+		if (error.code === '23505') {
+			res.status(400).json('Email Already Registered');
+		}
+		res.status(500).json({ message: error.message });
 	}
 }
 module.exports = {
