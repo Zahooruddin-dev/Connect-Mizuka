c61c94f936a43080314e9825457dc2323aa1e648
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-25 04:01:45 +0500

    Auth routes connceted to the app (server file)

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
-const PORT = process.env.PORT || 3000
-app.use(cors())
-app.use(express.json())
-app.get('/', (req,res)=>{res.json('Initiated')})
-app.listen(PORT, console.log('Server listening at 3000')
-)
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+const authRoutes = require('./Routes/authRoutes');
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth/', authRoutes);
+app.listen(PORT, '0.0.0.0', () => [console.log('Server listening at 3000')]);
