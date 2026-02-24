3cdfc5ec6f820c14d7798445b0f1397c3386daf7
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-25 03:55:42 +0500

    Auth query made for registration and login of the user' ' ' '

---
+++ backend/db/queryAuth.js
@@ -0,0 +1,18 @@
+async function loginQuery(email) {
+	const { rows } = await pool.query('SELECT * FROM users WHERE email = $1', [
+		email,
+	]);
+	return rows[0] || null;
+}
+async function registerQuery(username, email, password_hash, role = 'student') {
+	const { rows } = await pool.query(
+		`
+		INSERT INTO users (username,email,password_hash,role,institute_id) VALUES ($1,$2,$3,$4,$5) RETURNING id, username, email, role,institute_id`,
+		[username, email, password_hash, role],
+	);
+	return rows[0];
+}
+module.exports = {
+	loginQuery,
+	registerQuery,
+};
