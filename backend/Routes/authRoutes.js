485f4ec0547f481a9a97fb8d811704f2ecc083c9
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-25 05:55:12 +0500

    Reset password request sent route created

--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../Controller/AuthController');
+const resetController = require('../Controller/ResetController');
 router.post('/login', authController.Login);
 router.post('/register', authController.Register);
+router.post('/request-reset', resetController.requestPasswordReset);
 module.exports = router;
