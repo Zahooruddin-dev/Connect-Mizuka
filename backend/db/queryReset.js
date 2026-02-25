185786c4453e2254022b56c0c376500cbfe93296
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-25 05:38:08 +0500

    delete reset  code query added

--- backend/db/queryReset.js
+++ backend/db/queryReset.js
@@ -4,10 +4,14 @@ async function saveResetCode(email, code, expires) {
 }
 async function verifyResetCode(email, code) {
 	const query = `SELECT FROM * password_resets WHERE email =$1 AND code =$2 AND expires_at>NOW()`;
-	const {rows} = await pool.query(query,[email,code])
-	return rows[0]|| null
+	const { rows } = await pool.query(query, [email, code]);
+	return rows[0] || null;
+}
+async function deleteResetCode(email) {
+	await pool.query(`DELETE FROM password_resets WHERE email =$1`, [email]);
 }
 module.exports = {
 	saveResetCode,
 	verifyResetCode,
+	deleteResetCode,
 };
