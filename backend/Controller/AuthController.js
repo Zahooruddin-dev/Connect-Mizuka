dd6d1ed0ea0c3966304b82fcd49a3688463d8962
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-25 05:17:09 +0500

    Login system created, now user should be able to login and also JWT initited with expiry for it being 1 day

--- backend/Controller/AuthController.js
+++ backend/Controller/AuthController.js
@@ -3,10 +3,41 @@ const jwt = require('jsonwebtoken');
 const db = require('../db/queryAuth');
 
 async function Login(req, res) {
+	const { email, password } = req.body;
+
 	try {
-		res.status(200).json('sadsadsa Server Error');
+		const user = db.loginQuery(email);
+		if (!user) {
+			res.status(401).json({ message: 'Invalid email or password' });
+		}
+		const match = await bcypt.compare(password, user.password_hash);
+		if (!match) {
+			res.status(401).json({ message: 'Invalid email or password' });
+		}
+		const token = jwt.sign(
+			{
+				id: user.id,
+				email: user.role,
+				username: user.username,
+				createdAt: user.created_at,
+				institute_id: user.institute_id,
+			},
+			process.env.JWT_SECRET,
+			{ expiresIn: '1d' },
+		);
+		res.status(200).json({
+			message: 'Login Successful',
+			token,
+			user: {
+				id: user.id,
+				email: user.role,
+				username: user.username,
+				createdAt: user.created_at,
+				institute_id: user.institute_id,
+			},
+		});
 	} catch (error) {
-		res.status(500).json('Internal Server Error', error);
+		res.status(500).json({ message: error.message });
 	}
 }
 async function Register(req, res) {
