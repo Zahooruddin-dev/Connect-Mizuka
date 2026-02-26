06001b4fbd7f70ba1c32f74833155154f5fec072
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-27 02:33:47 +0500

    Get chat history controller added

---
+++ backend/Controller/messageController.js
@@ -0,0 +1,10 @@
+const db = require('../db/queryMessage')
+async function getChatHistory(req,res) {
+  const {channelId} = req.params
+  try {
+    const chat = await db.getChatHistoryQuery(channelId)
+    res.status(200).json(chat)
+  } catch (error) {
+    res.status(500).json({error:'Failed to load messages'})
+  }
+}
