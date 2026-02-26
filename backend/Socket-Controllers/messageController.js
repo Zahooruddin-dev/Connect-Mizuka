dfcd6b619a084510f9194b39687106b767bce38e
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-26 05:06:21 +0500

    handle send message controller added

--- backend/Socket-Controllers/messageController.js
+++ backend/Socket-Controllers/messageController.js
@@ -1,10 +1,23 @@
-const db = require('../db/querySocketMessage')
-async function handleSendMessage(req,res) {
-  const {channel_id,sender_id,message} = data
-  try {
+const db = require('../db/querySocketMessage');
+async function handleSendMessage(socket,io,data) {
+	const { channel_id, sender_id, message } = data;
+	try {
+		const savedMessage = await db.saveSentMessages(
+			channel_id,
+			sender_id,
+			message,
+		);
+		io.to(channel_id).emit('receive_message', {
+			id: savedMessage.id,
+			text: savedMessage.content,
+			from: savedMessage.sender_id,
+			timestamp: savedMessage.created_at,
+		});
+    console.log(`Message saved to the DB and sent to room: ${channel_id}`);
     
-  } catch (error) {
-    
+	} catch (error) {
+    console.error('Db insert error: ',error.message)
+    socket.emit('error',{message:'Failed to send message'})
   }
 }
-module.exports= {handleSendMessage}
+module.exports = { handleSendMessage };
