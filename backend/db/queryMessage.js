618077782c66a0bca1684e7a044154c7102cb4f7
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-27 02:50:26 +0500

    Get a single message query added so we can get a single message from DB

--- backend/db/queryMessage.js
+++ backend/db/queryMessage.js
@@ -1,19 +1,30 @@
-const pool = require('./Pool')
+const pool = require('./Pool');
 async function getChatHistoryQuery(channel_id) {
-  const query = `SELECT m.id, m.content,u.username,m.sender_id
+	const query = `SELECT m.id, m.content,u.username,m.sender_id
     FROM messages m
     JOIN users u ON m.sender_id = u.id
     WHERE m.channel_id = $1
     ORDER BY m.created_at ASC
-  `
-  const {rows} = await pool.query(query,[channel_id])
-  return rows || null 
-}
-async function deleteMessage(messageId, userId) {
-  const query = `DELETE FROM messages where id =$1 AND sender_id =$2
-  RETURNING *`
-  const {rows} = await pool.query(query,[messageId, userId])
-  return rows || null 
+  `;
+	const { rows } = await pool.query(query, [channel_id]);
+	return rows || null;
 }
 
-module.exports = {getChatHistoryQuery,deleteMessage}
+async function getSingleMessageQuery(messageId) {
+	const query = `SELECT m.* u.username FROM messages m JOIN users u ON m.sender_id = u.id WHERE m.id = $1`;
+	const { rows } = await pool.query(query, [messageId]);
+	return rows || null;
+}
+
+async function deleteMessageQuery(messageId, userId) {
+	const query = `DELETE FROM messages where id =$1 AND sender_id =$2
+  RETURNING *`;
+	const { rows } = await pool.query(query, [messageId, userId]);
+	return rows || null;
+}
+
+module.exports = {
+	getChatHistoryQuery,
+	getSingleMessageQuery,
+	deleteMessageQuery,
+};
