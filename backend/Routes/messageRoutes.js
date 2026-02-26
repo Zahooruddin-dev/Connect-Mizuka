88f624a00ec9ed2bf87f44da7ea93d231649da9c
zahooruddin-dev <mzkhan886@gmail.com>
2026-02-27 03:12:51 +0500

    Delete channel and message routes added

--- backend/Routes/messageRoutes.js
+++ backend/Routes/messageRoutes.js
@@ -2,4 +2,7 @@ const express = require('express');
 const router = express.Router();
 const messageController = require('../Controller/messageController');
 router.get('/:channelId', messageController.getChatHistory);
+router.delete('/channel/:channelId', messageController.deleteChannel);
+router.delete('/message/:messageId', messageController.deleteMessage);
+
 module.exports = router;
