f8315429443d230437affe0529b810c61edc3802
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-01 04:21:12 +0500

    Create default cahnnel added

--- backend/db/queryInstitute.js
+++ backend/db/queryInstitute.js
@@ -1,7 +1,18 @@
 const pool = require('./Pool');
-async function createInstitute(name,adminId) {
-  const {rows} = await pool.query(`
-    INSERT INTO institutes (name) VALUES ($1) RETURNING id,name`,[name])
-    return rows[0]
+async function createInstitute(name) {
+	const { rows } = await pool.query(
+		`
+    INSERT INTO institutes (name) VALUES ($1) RETURNING id,name`,
+		[name],
+	);
+	return rows[0];
 }
-module.exports={createInstitute}
+async function createDefaultChannel(newInstituteId) {
+	const { rows } = await pool.query(
+		`
+    INSERT INTO channels (name,institute_id, is_private) VALUES ($1,$2,$3) `,
+		['General Hallway', newInstituteId, false],
+	);
+	return rows[0];
+}
+module.exports = { createInstitute, createDefaultChannel };
