3fea6dbee513e90ebcb5219ecf9c6a90bbdb2fa1
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-04 06:09:12 +0500

    Delete and put routes added for channel

--- backend/Routes/channelRoutes.js
+++ backend/Routes/channelRoutes.js
@@ -5,5 +5,7 @@ const channelController = require('../Controller/channelController');
 router.post('/create', channelController.createChannel);
 router.get('/institute/:instituteId', channelController.getChannelsForInstitute);
 router.get('/:channelId', channelController.getChannelById);
+router.put('/:channelId', channelController.updateChannel);
+router.delete('/:channelId', channelController.deleteChannelById);
 
 module.exports = router;
