8e0fd9895b32c26ea3a46e34efa6b8c0179e88b5
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-05 10:22:10 +0500

    ADDED controller and made changes in query p2p as u1 and u2 we were sednign were not objects

--- backend/Controller/p2pController.js
+++ backend/Controller/p2pController.js
@@ -1,6 +1,23 @@
-const db = require('../db/queryP2P')
-async function getOrCreateChatroom(req,res) {
-  const {user1 , user2 } = req.body
-  const [u1,u2] = [user1,user2].sort()
+const db = require('../db/queryP2P');
+async function getOrCreateChatroom(req, res) {
+	const { user1, user2 } = req.body;
+	const [u1, u2] = [user1, user2].sort();
+	try {
+		const existingRoom = await db.findExistingRoomQuery(u1, u2);
+		if (existingRoom) {
+			res.status(200).json({
+				chatroom: existingRoom,
+				isNew: false,
+			});
+		}
+		const newRoom = await db.createNewRoom(u1, u2);
+    return res.status(201).json({ 
+      chatroom: newRoom, 
+      isNew: true 
+    });
+	} catch (error) {
+		console.error('P2P Room Error:', err);
+		res.status(500).json({ error: 'Could not initialize private chat' });
+	}
 }
-module.exports= {getOrCreateChatroom}
+module.exports = { getOrCreateChatroom };
--- backend/db/queryP2P.js
+++ backend/db/queryP2P.js
@@ -1,14 +1,16 @@
 const pool = require('./Pool');
-async function findExistingRoomQuery({ u1, u2 }) {
-	await pool.query(
+async function findExistingRoomQuery(u1,u2) {
+	const { rows } = await pool.query(
 		`SELECT * FROM p2p_chatrooms WHERE (user_one_id = $1 AND user_two_id =$2)`,
 		[u1, u2],
 	);
+	return rows[0] || null;
 }
-async function createNewRoom({ u1, u2 }) {
-	await pool.query(
+async function createNewRoom(u1,u2) {
+	const { rows } = await pool.query(
 		`INSERT INTO p2p_chatrooms (user_one_id, user_two_id) VALUES ($1,$2) RETURNING *`,
-		[u1, u2]
+		[u1, u2],
 	);
+	return rows[0] || null;
 }
 module.exports = { findExistingRoomQuery, createNewRoom };
