e87c7ae1ac2cc0fc9ad133c702468c1c12460e6c
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-08 02:39:31 +0500

    unread userid route added in p2p routes

--- backend/Routes/p2pRoutes.js
+++ backend/Routes/p2pRoutes.js
@@ -4,5 +4,6 @@ const p2pController = require('../Controller/p2pController');
 
 router.post('/room', p2pController.getOrCreateChatroom);
 router.get('/messages/:roomId', p2pController.getMessages);
+router.get('/unread/:userId', p2pController.getUnreadCounts);
 
 module.exports = router;
