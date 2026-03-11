5d161206f8e57f9c9e9a985c18af0dff009875cd
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-11 09:57:03 +0500

    get institue memebr route  added

--- backend/Routes/instituteRoutes.js
+++ backend/Routes/instituteRoutes.js
@@ -5,5 +5,6 @@ router.post('/create', instituteController.createInstitute);
 router.get('/key/:adminId', instituteController.getGlobalKey);
 router.get('/dashboard/:adminId', instituteController.getAdminDashboard);
 router.get('/:instituteId/search-members', instituteController.searchMembers);
+router.get('/:instituteId/institute-members', instituteController.getInstituteMembers);
 
 module.exports = router;
