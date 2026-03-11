9931b16badb6e530e73a5ec68293cd7b7953e6ea
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-11 10:13:06 +0500

    Serach channle messages route added

--- backend/Routes/channelRoutes.js
+++ backend/Routes/channelRoutes.js
@@ -5,6 +5,7 @@ const channelController = require('../Controller/channelController');
 router.post('/create', channelController.createChannel);
 router.get('/institute/:instituteId', channelController.getChannelsForInstitute);
 router.get('/:channelId', channelController.getChannelById);
+router.get('/:channelId/search-messages', channelController.searchChannelMessages);
 router.put('/:channelId', channelController.updateChannel);
 router.delete('/:channelId', channelController.deleteChannelById);
 
