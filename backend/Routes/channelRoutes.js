8f77371a9aee0447c2202edb3be97788e8aaf934
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-12 10:24:40 +0500

    verify token added to all routes in channel routes

--- backend/Routes/channelRoutes.js
+++ backend/Routes/channelRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { verifyToken } = require('../middleware/authMiddleware');
 const channelController = require('../Controller/channelController');
+
+router.use(verifyToken);
 
 router.post('/create', channelController.createChannel);
 router.get('/institute/:instituteId', channelController.getChannelsForInstitute);
