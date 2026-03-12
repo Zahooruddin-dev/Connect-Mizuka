b519abdae5e3381e63f94ef31c4abc3095979d76
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-12 10:40:03 +0500

    restrictToAdmin middlware beign used in institute routes

--- backend/Routes/instituteRoutes.js
+++ backend/Routes/instituteRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const instituteController = require('../Controller/instituteController');
-const { verifyToken } = require('../middleware/authMiddleware');
+const { verifyToken, restrictToAdmin } = require('../middleware/authMiddleware');
 
 router.use(verifyToken);
 
-router.post('/create', instituteController.createInstitute);
-router.get('/dashboard', instituteController.getAdminDashboard); 
-router.get('/key/:instituteId', instituteController.getGlobalKey); 
+router.post('/create', restrictToAdmin, instituteController.createInstitute);
+router.get('/dashboard', restrictToAdmin, instituteController.getAdminDashboard); 
+router.get('/key/:instituteId', restrictToAdmin, instituteController.getGlobalKey); 
+
 router.get('/:instituteId/search-members', instituteController.searchMembers);
 router.get('/:instituteId/institute-members', instituteController.getInstituteMembers);
 
