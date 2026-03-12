0ea5819b0f53eb648eb37f4aaa4e730e0f9ef59c
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-12 10:41:41 +0500

    restrictToAdmin middlware beign used in messages routes

--- backend/Routes/messageRoutes.js
+++ backend/Routes/messageRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const messageController = require('../Controller/messageController');
-const channelController = require('../Controller/channelController');
+const { verifyToken, restrictToAdmin } = require('../middleware/authMiddleware');
+
+router.use(verifyToken);
+
 router.get('/:channelId', messageController.getChatHistory);
-router.delete('/channel/:channelId', channelController.deleteChannelById);
 router.delete('/message/:messageId', messageController.deleteMessage);
+
+router.delete('/channel/:channelId', restrictToAdmin, messageController.deleteChannel);
 
 module.exports = router;
