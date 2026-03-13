5736a39b1092076b369075c5bf2543c7c1969c50
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-13 10:56:33 +0500

    Pool database urle changes

--- backend/db/Pool.js
+++ backend/db/Pool.js
@@ -1,8 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: true,
-	},
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction 
+    ? { rejectUnauthorized: false } 
+    : false,
 });
