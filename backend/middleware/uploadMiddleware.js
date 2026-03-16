c8b742206569f5ae49bd645137c2e5eb1d418ef7
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-16 16:13:19 +0500

    Added upload middlware for profile picture"s for users

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,29 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'mizuka_uploads', 
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp','avif'],
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Not an image! Please upload an image.'), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
