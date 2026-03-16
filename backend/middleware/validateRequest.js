848051d60067d67b78fa2629222c07e72858abd4
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-16 12:23:44 +0500

    Small improvements added to validate requests

--- backend/middleware/validateRequest.js
+++ backend/middleware/validateRequest.js
@@ -1,17 +1,17 @@
+const { ZodError } = require('zod');
+
 const validate = (schema) => (req, res, next) => {
-	try {
-		schema.parse(req.body);
-		next();
-	} catch (error) {
-		if (error.errors) {
-			const errorMessage = error.errors.map((err) => err.message).join(', ');
-			return res.status(400).json({ message: errorMessage });
-		}
-		console.error('Validation Middleware Error:', error);
-		return res
-			.status(500)
-			.json({ message: 'Internal Server Error during validation' });
-	}
+  try {
+    schema.parse(req.body);
+    next();
+  } catch (error) {
+    if (error instanceof ZodError) {
+      const errorMessage = error.issues.map((err) => err.message).join(', ');
+      return res.status(400).json({ message: errorMessage });
+    }
+        console.error("SYSTEM ERROR:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 module.exports = validate;
