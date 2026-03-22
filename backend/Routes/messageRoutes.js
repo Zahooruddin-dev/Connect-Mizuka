0ca26452e304548f98c17081602a75784aba47bd
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-23 02:28:18 +0500

    Upload audio route added (feat :: message routes)

--- backend/Routes/messageRoutes.js
+++ backend/Routes/messageRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const messageController = require('../Controller/messageController');
 const { verifyToken, restrictToAdmin } = require('../middleware/authMiddleware');
-
+const upload = require('../middleware/uploadMiddleware');
 router.use(verifyToken);
 
+router.post('/upload-audio', upload.single('audio'), messageController.uploadAudioFile);
 router.get('/:channelId', messageController.getChatHistory);
 router.delete('/message/:messageId', messageController.deleteMessage);
 router.delete('/channel/:channelId', restrictToAdmin, messageController.deleteChannel);
