ac534e212341f77fa21cd0faa95ef52e6cabf21f
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-23 02:35:53 +0500

    New audio upload middlware implemented in message routes

--- backend/Routes/messageRoutes.js
+++ backend/Routes/messageRoutes.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const messageController = require('../Controller/messageController');
-const { verifyToken, restrictToAdmin } = require('../middleware/authMiddleware');
-const upload = require('../middleware/uploadMiddleware');
+const {
+	verifyToken,
+	restrictToAdmin,
+} = require('../middleware/authMiddleware');
+const uploadAudio = require('../middleware/audioUploadMiddleware');
 router.use(verifyToken);
 
-router.post('/upload-audio', upload.single('audio'), messageController.uploadAudioFile);
+router.post(
+	'/upload-audio',
+	uploadAudio.single('audio'),
+	messageController.uploadAudioFile,
+);
+
 router.get('/:channelId', messageController.getChatHistory);
 router.delete('/message/:messageId', messageController.deleteMessage);
-router.delete('/channel/:channelId', restrictToAdmin, messageController.deleteChannel);
+router.delete(
+	'/channel/:channelId',
+	restrictToAdmin,
+	messageController.deleteChannel,
+);
 
 module.exports = router;
