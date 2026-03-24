b383b8a07f294b2bac20bbcbdd1c805e9080442b
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-24 16:11:19 +0500

    WEB RTC SIGNALLING

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,7 @@ const channelRoutes = require('./Routes/channelRoutes');
 const p2pRoutes = require('./Routes/p2pRoutes');
 const socketController = require('./Socket-Controllers/messageController');
 const p2pSocketController = require('./Socket-Controllers/P2psocketcontroller');
+const { default: socket } = require('../frontend/src/services/socket');
 
 const PORT = process.env.PORT || 3000;
 
@@ -199,6 +200,23 @@ io.on('connection', (socket) => {
 			message: 'Backend received your test!',
 			timestamp: new Date().toISOString(),
 		});
+	});
+});
+
+// WEB RTC SIGNALING
+io.on('connection', (socket) => {
+	console.log(`User connected: ${socket.id}`);
+	socket.on('call:user', ({ to, offer }) => {
+		socket.to(to).emit('call:incoming', { from: socket.id, offer });
+	});
+	socket.on('call:accepted', ({ to, offer }) => {
+		socket.to(to).emit('call:answered', { from: socket.id, offer });
+	});
+	socket.on('ice-candidate', ({ to, candidate }) => {
+		socket.to(to).emit('ice-candidate', { from: socket.id, candidate });
+	});
+	socket.on('call:end', ({ to }) => {
+		socket.to(to).emit('call:ended');
 	});
 });
 
